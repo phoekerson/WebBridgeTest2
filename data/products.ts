dcd21a0d5152f4a2de3bfaadde8be617bad3bb1a
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-05-17 17:59:45 +0200

    Adding product

---
+++ data/products.ts
@@ -0,0 +1,14 @@
+import { Product } from '../types';
+
+export const PRODUCTS: Product[] = [
+  { id: 1, name: 'Product 1', price: 100, image: '/images/product1.jpg' },
+  { id: 2, name: 'Product 2', price: 120, image: '/images/product2.jpg' },
+  { id: 3, name: 'Product 3', price: 90, image: '/images/product3.jpg' },
+  { id: 4, name: 'Product 4', price: 150, image: '/images/product4.jpg' },
+  { id: 5, name: 'Product 5', price: 200, image: '/images/product5.jpg' },
+  { id: 6, name: 'Product 6', price: 80, image: '/images/product6.jpg' },
+  { id: 7, name: 'Product 7', price: 70, image: '/images/product7.jpg' },
+  { id: 8, name: 'Product 8', price: 110, image: '/images/product8.jpg' },
+  { id: 9, name: 'Product 9', price: 140, image: '/images/product9.jpg' },
+  { id: 10, name: 'Product 10', price: 130, image: '/images/product10.jpg' },
+];
